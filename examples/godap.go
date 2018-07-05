bfc1627a484d7b5a4abd7eb3b636796e5bb96043
Jacob Nerney <jnerney@kenzan.com>
2018-07-05 13:33:51 -0400

    added example and tested functionality

--- examples/godap.go
+++ examples/godap.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"godap"
+	"log"
+)
+
 func main() {
 
+	//create a new client
+	client := godap.New(godap.Options{
+		BaseDN:   "CN=Users,DC=Company",
+		Filter:   "sAMAccountName",
+		Password: "username",
+		Username: "password",
+		URL:      "ldap.directory.com:389",
+	})
+
+	//username and password to authenticate
+	username := "jdoe"
+	password := "pass1234"
+
+	//attempt the authentication
+	err := client.Authenticate(username, password)
+
+	//see the results
+	if err != nil {
+		log.Print(err)
+	} else {
+		log.Print("user successfully authenticated!")
+	}
 }
