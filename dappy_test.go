7405fc347ce33264f8c20e9ca5b014dfa7cb364c
Jacob Nerney <nerney@users.noreply.github.com>
2019-06-02 05:19:34 -0400

    switched to mod, got rid of entry function, auth only, some refactoring (#5)

--- dappy_test.go
+++ dappy_test.go
@@ -1,154 +1,61 @@
-package dappy
+package dappy_test
 
 import (
 	"testing"
 
+	"github.com/nerney/dappy"
 	"github.com/stretchr/testify/assert"
 )
 
+var client, _ = dappy.New(dappy.Config{
+	BaseDN: "dc=example,dc=com",
+	Filter: "uid",
+	ROUser: dappy.User{Name: "cn=read-only-admin,dc=example,dc=com", Pass: "password"},
+	Host:   "ldap.forumsys.com:389",
+})
+
 func TestDappyAuth_HappyPath(t *testing.T) {
-	client := New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "uid",
-		BasePassword: "password",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
-	})
-	assert.Nil(t, client.Authenticate("tesla", "password"),
+	assert.Nil(t, client.Auth("tesla", "password"),
 		"should authenticate successfully")
 }
 
-func TestDappyAuth_BindFail(t *testing.T) {
-	client := New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "uid",
-		BasePassword: "badpassword",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
+func TestDappyAuth_InitialBindFail(t *testing.T) {
+	_, err := dappy.New(dappy.Config{
+		BaseDN: "dc=example,dc=com",
+		Filter: "uid",
+		ROUser: dappy.User{Name: "cn=read-only-admin,dc=example,dc=com", Pass: "badpassword"},
+		Host:   "ldap.forumsys.com:389",
 	})
-	err := client.Authenticate("tesla", "password")
-	assert.Equal(t, "godap: could not perform initial bind", err.Error(),
+	assert.Equal(t, "LDAP Result Code 49 \"Invalid Credentials\": ", err.Error(),
 		"should fail initial bind")
 }
 
 func TestDappyAuth_EmptyPassword(t *testing.T) {
-	client := New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "uid",
-		BasePassword: "password",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
-	})
-	err := client.Authenticate("tesla", "")
-	assert.Equal(t, "godap: empty password", err.Error(),
+	err := client.Auth("tesla", "")
+	assert.Equal(t, "LDAP Result Code 206 \"Empty password not allowed by the client\": ldap: empty password not allowed by the client", err.Error(),
 		"should fail because of empty password")
 }
-func TestDappyAuth_EmptyUsername(t *testing.T) {
-	client := New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "uid",
-		BasePassword: "password",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
-	})
-	err := client.Authenticate("", "password")
-	assert.Equal(t, "godap: empty username", err.Error(),
-		"should fail because of empty username")
-}
 
-func TestDappyAuth_FailSearch(t *testing.T) {
-	client := New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "(",
-		BasePassword: "password",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
+func TestDappyAuth_FailBadFilter(t *testing.T) {
+	client, _ := dappy.New(dappy.Config{
+		BaseDN: "dc=example,dc=com",
+		Filter: "(",
+		ROUser: dappy.User{Name: "cn=read-only-admin,dc=example,dc=com", Pass: "password"},
+		Host:   "ldap.forumsys.com:389",
 	})
-	err := client.Authenticate("tesla", "password")
-	assert.Equal(t, "godap: error performing search", err.Error(),
+	err := client.Auth("tesla", "password")
+	assert.Equal(t, "LDAP Result Code 201 \"Filter Compile Error\": ldap: unexpected end of filter", err.Error(),
 		"should fail to perform search")
 }
 
-func TestDappyAuth_NotFound(t *testing.T) {
-	client := New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "uid",
-		BasePassword: "password",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
-	})
-	err := client.Authenticate("daddy", "password")
-	assert.Equal(t, "godap: user not found in directory", err.Error(),
+func TestDappyAuth_UserNotFound(t *testing.T) {
+	err := client.Auth("daddy", "password")
+	assert.Equal(t, "not found", err.Error(),
 		"should fail to find user")
 }
 
-func TestDappyAuth_NoAuth(t *testing.T) {
-	client := New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "uid",
-		BasePassword: "password",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
-	})
-	err := client.Authenticate("tesla", "wrongpassword")
-	assert.Equal(t, "godap: could not authenticate user", err.Error(),
+func TestDappyAuth_FailAuth(t *testing.T) {
+	err := client.Auth("tesla", "wrongpassword")
+	assert.Equal(t, "LDAP Result Code 49 \"Invalid Credentials\": ", err.Error(),
 		"should fail to authenticate user")
-}
-
-func TestDappy_NoConnection(t *testing.T) {
-	defer func() {
-		if r := recover(); r != nil {
-			err := r.(error)
-			if err.Error() != "godap: could not establish connection" {
-				panic(err)
-			}
-		} else {
-			panic("should panic")
-		}
-	}()
-	New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "uid",
-		BasePassword: "password",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.isnotarealthing.com:389",
-	})
-}
-
-func TestDappyGetEntry(t *testing.T) {
-	client := New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "uid",
-		BasePassword: "password",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
-	})
-	_, err := client.GetUserEntry("tesla")
-	assert.Nil(t, err, "should get entry")
-	_, err = client.GetUserEntry("daddy")
-	assert.Equal(t, "godap: user not found in directory", err.Error(),
-		"should fail to find user")
-	_, err = client.GetUserEntry("")
-	assert.Equal(t, "godap: empty username", err.Error(),
-		"should fail because of empty username")
-	client = New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "(",
-		BasePassword: "password",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
-	})
-	_, err = client.GetUserEntry("tesla")
-	assert.Equal(t, "godap: error performing search", err.Error(),
-		"should fail to perform search")
-	client = New(Options{
-		BaseDN:       "dc=example,dc=com",
-		Filter:       "uid",
-		BasePassword: "badpassword",
-		BaseUser:     "cn=read-only-admin,dc=example,dc=com",
-		URL:          "ldap.forumsys.com:389",
-	})
-	_, err = client.GetUserEntry("tesla")
-	assert.Equal(t, "godap: could not perform initial bind", err.Error(),
-		"should fail initial bind")
 }
